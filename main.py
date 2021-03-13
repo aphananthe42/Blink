c3acd8e8c7e3b461c4cc5c5554d941324376cca7
Ryota Miyazaki <>
2021-03-14 02:45:01 +0900

    sos

---
+++ main.py
@@ -0,0 +1,31 @@
+from machine import Pin
+from time    import sleep
+
+led_onboard = Pin(25, Pin.OUT)
+
+while True:
+    for s in range(3):
+        led_onboard.on()
+        sleep(0.2)
+        led_onboard.off()
+        sleep(0.1)
+    led_onboard.off()
+    sleep(0.5)
+
+    for o in range(3):
+        led_onboard.on()
+        sleep(1)
+        led_onboard.off()
+        sleep(0.1)
+    led_onboard.off()
+    sleep(0.5)
+
+    for s in range(3):
+        led_onboard.on()
+        sleep(0.2)
+        led_onboard.off()
+        sleep(0.1)
+
+    led_onboard.off()
+    sleep(10.0)
+    
